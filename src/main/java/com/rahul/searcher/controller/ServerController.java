743c91c1a1e7a2ca2d5e42bcfefed35e919c96ce
rahulvatsa <vatsa.rahul.999@gmail.com>
2018-10-08 02:03:27 +0530

    Added first contoller

---
+++ src/main/java/com/rahul/searcher/controller/ServerController.java
@@ -0,0 +1,11 @@
+package com.rahul.searcher.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/server")
+public class ServerController {
+
+	
+}
