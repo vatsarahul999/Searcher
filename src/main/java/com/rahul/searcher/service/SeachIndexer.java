901797b31fe3d35b5e6edb0aa62c525456df25b4
rahulvatsa <vatsa.rahul.999@gmail.com>
2018-10-11 23:33:13 +0530

    Added Index Corrections

--- src/main/java/com/rahul/searcher/service/SeachIndexer.java
+++ src/main/java/com/rahul/searcher/service/SeachIndexer.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
+
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
@@ -19,6 +21,8 @@ import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.supercsv.cellprocessor.Optional;
 import org.supercsv.cellprocessor.ift.CellProcessor;
@@ -29,13 +33,21 @@ import org.supercsv.prefs.CsvPreference;
 @Service
 public class SeachIndexer {
 	private final Logger log = LoggerFactory.getLogger(SeachIndexer.class);
+	
+	private String srcFilePath;
+	
+	private String indexDirectoryPath;
+	
+	@Autowired
+	private Environment env;
+	
 
 	public void index(File indexFile) {
 		log.info("Indexing file {}", indexFile);
 		StandardAnalyzer analyzer = new StandardAnalyzer();
 		Directory index = null;
 		try {
-			index = FSDirectory.open(indexFile.toPath());
+			index = FSDirectory.open(new File(indexDirectoryPath).toPath());
 		} catch (Exception e) {
 			log.error("There was an error indexing files", e);
 		}
@@ -46,7 +58,7 @@ public class SeachIndexer {
 		config.setRAMPerThreadHardLimitMB(512);
 		IndexWriter w = null;
 		try {
-			try (ICsvMapReader listReader = new CsvMapReader(new FileReader(indexFile.getAbsolutePath()),
+			try (ICsvMapReader listReader = new CsvMapReader(new FileReader(indexFile),
 					CsvPreference.STANDARD_PREFERENCE)) {
 				// First Column is header names
 				final String[] headers = listReader.getHeader(true);
@@ -55,30 +67,56 @@ public class SeachIndexer {
 				Map<String, Object> fieldsInCurrentRow;
 				while ((fieldsInCurrentRow = listReader.read(headers, processors)) != null) {
 					w.addDocument(convertToDocument(fieldsInCurrentRow));
+					w.commit();
+					
 				}
+				w.flush();
 			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		try {
+			if(w!=null)
 			w.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+	@PostConstruct
+	public void init(){
+		srcFilePath = env.getProperty("search.index.inputFile");
+		log.info("The srcFile path is {}",srcFilePath);
+		indexDirectoryPath = env.getProperty("search.index.path");
+		File indexFile = createIfNotExists(srcFilePath);
+		createIfNotExists(indexDirectoryPath);
+		index(indexFile);
+		
+	}
+	public File createIfNotExists(String srcFilePath2) {
+		File indexFile = new File(srcFilePath2);
+		if (!indexFile.exists()) {
+			indexFile.mkdirs();
+			log.info("The index location is {}", indexFile.getAbsolutePath());
+		}
+		return indexFile;
+	}
 
 	private CellProcessor[] getProcessors() {
-		List<CellProcessor> list = new ArrayList<>();
-		for (String fString : SConstants.feilds)
-			list.add(new Optional());
-		return (CellProcessor[]) list.toArray();
+		CellProcessor[] res = new CellProcessor[SConstants.feilds.length];
+		for(int i = 0;i<SConstants.feilds.length;i++){
+			res[i] = new Optional();
+		}
+		return res;
 	}
 
 	private Iterable<? extends IndexableField> convertToDocument(Map<String, Object> fieldsInCurrentRow) {
 		Document document =  new Document();
-		for(String key: SConstants.feilds)
-		document.add(new StringField(key, fieldsInCurrentRow.get(key).toString(), Field.Store.YES));
+		for(String key: SConstants.feilds){
+		//	log.info("Added data to doc {} : {}",key,fieldsInCurrentRow.get(key).toString());
+		document.add(new StringField(key, fieldsInCurrentRow.get(key).toString().toLowerCase(), Field.Store.YES));
+		}
+		log.info("Indexed a doc");
 		return document;
 	}
 }
